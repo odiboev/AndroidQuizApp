5220943a18459083f246b941d3a7daa2c486d006
odiboev <odiboev@gmail.com>
2018-09-08 21:28:20 +0800

    SQLITE OPEN HELPER CLASS

--- app/src/main/java/com/example/bilal/awesomequizapp/QuizContract.java
+++ app/src/main/java/com/example/bilal/awesomequizapp/QuizContract.java
@@ -6,12 +6,12 @@ public final class QuizContract {
 
     private QuizContract(){}
 
-    public static class QuestionTable implements BaseColumns{
-        public static final String TABLE_NAME = "quiz_question";
-        public static final String COLUM_QUESTION = "questions";
-        public static final String COLUM_OPTION1 = "option1";
-        public static final String COLUM_OPTION2 = "option2";
-        public static final String COLUM_OPTION3 = "option3";
+    public static class QuestionsTable implements BaseColumns{
+        public static final String TABLE_NAME = "quiz_questions";
+        public static final String COLUMN_QUESTION = "questions";
+        public static final String COLUMN_OPTION1 = "option1";
+        public static final String COLUMN_OPTION2 = "option2";
+        public static final String COLUMN_OPTION3 = "option3";
         public static final String COLUMN_ANSWER_NR = "answer_nr";
 
         //TODO Nice explanation here: https://www.youtube.com/watch?v=XJTUwR4GxW0&t=7s
