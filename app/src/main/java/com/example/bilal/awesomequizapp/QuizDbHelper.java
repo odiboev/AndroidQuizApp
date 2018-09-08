1062fe8ecfe8e98b49f78d70a7e513c853cd796c
odiboev <odiboev@gmail.com>
2018-09-09 01:21:13 +0800

    RAWQUERY DATABASE WITH CURSOR

--- app/src/main/java/com/example/bilal/awesomequizapp/QuizDbHelper.java
+++ app/src/main/java/com/example/bilal/awesomequizapp/QuizDbHelper.java
@@ -2,9 +2,13 @@ package com.example.bilal.awesomequizapp;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import com.example.bilal.awesomequizapp.QuizContract.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class QuizDbHelper extends SQLiteOpenHelper {
     private static final String DATABASE_NAME = "MyAwesomeQuiz.db";
@@ -64,4 +68,27 @@ public class QuizDbHelper extends SQLiteOpenHelper {
         cv.put(QuestionsTable.COLUMN_ANSWER_NR, question.getAnswerNr());
         db.insert(QuestionsTable.TABLE_NAME, null, cv);
     }
+
+    public List<Question> getAllQuestions(){
+        List<Question> questionList = new ArrayList<>();
+        db = getReadableDatabase();
+        Cursor c = db.rawQuery("SELECT * FROM " + QuestionsTable.TABLE_NAME, null);
+
+        if(c.moveToFirst()){
+            do {
+                Question question = new Question();
+                question.setQuestion(c.getString(c.getColumnIndex(QuestionsTable.COLUMN_QUESTION)));
+                question.setOption1(c.getString(c.getColumnIndex(QuestionsTable.COLUMN_OPTION1)));
+                question.setOption2(c.getString(c.getColumnIndex(QuestionsTable.COLUMN_OPTION2)));
+                question.setOption3(c.getString(c.getColumnIndex(QuestionsTable.COLUMN_OPTION3)));
+                question.setAnswerNr((c.getInt(c.getColumnIndex(QuestionsTable.COLUMN_ANSWER_NR))));
+                questionList.add(question);
+            }while(c.moveToNext());
+        }
+
+        c.close();
+        return questionList;
+    }
+
+
 }
