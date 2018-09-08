a279ad6824a7bd6038a51e802230ba5ef5a9d117
odiboev <odiboev@gmail.com>
2018-09-08 19:18:24 +0800

    Completed first part part of the tutorial: PREPARING THE LAYOUTS

--- app/src/main/java/com/example/bilal/awesomequizapp/StartingScreenActivity.java
+++ app/src/main/java/com/example/bilal/awesomequizapp/StartingScreenActivity.java
@@ -1,7 +1,10 @@
 package com.example.bilal.awesomequizapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class StartingScreenActivity extends AppCompatActivity {
 
@@ -9,5 +12,19 @@ public class StartingScreenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_starting_screen);
+
+        Button buttonStartQuiz = findViewById(R.id.button_start_quiz);
+        buttonStartQuiz.setOnClickListener(new View.OnClickListener() {
+            //Annonymous inner class
+            @Override
+            public void onClick(View v) {
+                startQuiz();
+            }
+        });
+    }
+
+    private void startQuiz(){
+        Intent intent = new Intent(StartingScreenActivity.this, QuizActivity.class);
+        startActivity(intent);
     }
 }
